b1798b309d2f3c147a421594cdb6cb3d811086fc
Dragomegak <sle4@myseneca.ca>
2019-01-19 18:14:35 -0500

    Add files via upload
    Main window created, button somewhat working, some text showing

---
+++ Project/desm/desm/Menu.cpp
@@ -0,0 +1,187 @@
+//Required libraries
+#include <windows.h>
+#include <stdlib.h>
+#include <string.h>
+#include <tchar.h>
+
+// Global variables
+
+// The main window class name.
+static TCHAR DESM[] = _T("Desktop Environment Setup Manager");
+
+// The string that appears in the application's title bar.
+static TCHAR windowsTitle[] = _T("Window1");
+
+HINSTANCE hInst;
+HWND createAccountButton;
+
+// Forward declarations of functions included in this code module:
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+//Required windows-procedure function
+//Code to handle messages that the application receives from Windows when events occur here
+LRESULT CALLBACK WndProc(
+	_In_ HWND   hwnd,
+	_In_ UINT   uMsg,
+	_In_ WPARAM wParam,
+	_In_ LPARAM lParam
+);
+
+//Required int main for the project
+int CALLBACK WinMain(
+	_In_ HINSTANCE hInstance,
+	_In_ HINSTANCE hPrevInstance,
+	_In_ LPSTR     lpCmdLine,
+	_In_ int       nCmdShow
+
+) {
+	WNDCLASSEX wcex;
+
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(hInstance, IDI_APPLICATION);
+	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.lpszMenuName = NULL;
+	wcex.lpszClassName = DESM;
+	wcex.hIconSm = LoadIcon(wcex.hInstance, IDI_APPLICATION);
+
+	if (!RegisterClassEx(&wcex))
+	{
+		MessageBox(NULL,
+			_T("Call to RegisterClassEx failed!"),
+			_T("Window1"),
+			NULL);
+
+		return 1;
+	}
+
+	// Store instance handle in our global variable
+	hInst = hInstance;
+
+
+	// The parameters to CreateWindow explained:
+	// DESM: the name of the application
+	// windowsTitle: the text that appears in the title bar
+	// WS_OVERLAPPEDWINDOW: the type of window to create
+	// CW_USEDEFAULT, CW_USEDEFAULT: initial position (x, y)
+	// 500, 100: initial size (width, length)
+	// NULL: the parent of this window
+	// NULL: this application does not have a menu bar
+	// hInstance: the first parameter from WinMain
+	// NULL: not used in this application
+	HWND hWnd = CreateWindow(
+		DESM,
+		windowsTitle,
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		500, 500,
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+	);
+
+
+	if (!hWnd)
+	{
+		MessageBox(NULL,
+			_T("Call to CreateWindow failed!"),
+			_T("Window1"),
+			NULL);
+
+		return 1;
+	}
+
+	// The parameters to ShowWindow explained:
+	// hWnd: the value returned from CreateWindow
+	// nCmdShow: the fourth parameter from WinMain
+	ShowWindow(hWnd,
+		nCmdShow);
+	UpdateWindow(hWnd);
+
+	// Main message loop:
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return (int)msg.wParam;
+}
+//testing createing a button proc
+WNDPROC createButtonProc;
+
+//callback for buttonproc
+LRESULT CALLBACK ButtonProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp)
+{
+	switch (msg) {
+	case WM_LBUTTONDOWN:
+		return 0;
+	case WM_LBUTTONUP:
+		return 0;
+	}
+	return CallWindowProc(createButtonProc, hwnd, msg, wp, lp);
+}
+
+
+//  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  PURPOSE:  Processes messages for the main window.
+//
+//  WM_PAINT    - Paint the main window
+//  WM_DESTROY  - post a quit message and return
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	PAINTSTRUCT ps;
+	HDC hdc;
+	//TCHAR greeting[] = _T("Hello, Windows desktop!");
+	TCHAR username[] = _T("Username:");
+	TCHAR password[] = _T("Password:");
+
+	switch (message)
+	{
+	case WM_CREATE:
+	{
+		HWND createAccountButton = CreateWindow(TEXT("button"), TEXT("Button"),
+			WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,
+			80, 10, 100, 50,
+			hWnd, NULL, NULL, NULL);
+		createButtonProc = (WNDPROC)SetWindowLong(createAccountButton, GWL_WNDPROC, (LONG)ButtonProc);
+
+		break;
+	}
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+
+		// Here your application is laid out.
+		// For this introduction, we just print out "Hello, Windows desktop!"
+		// in the top left corner.
+		//TextOut(hdc,
+			//5, 5,
+			//greeting, _tcslen(greeting));
+		// End application-specific layout section.
+		TextOut(hdc, 
+			10, 200,
+			username, _tcsclen(username));
+		TextOut(hdc,
+			10, 220,
+			password, _tcsclen(password));
+
+		EndPaint(hWnd, &ps);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+		break;
+	}
+
+	return 0;
+}
